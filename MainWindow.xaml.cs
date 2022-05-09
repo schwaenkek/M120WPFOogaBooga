a612cb2a25e14af5eda30f42f250cc06cb17fe62
Schwaen <91459462+schwaenkek@users.noreply.github.com>
2022-05-09 14:40:45 +0200

    kek

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,29 @@ namespace M120WPFOogaBooga
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+
+            txt_output.Text = "";
+            try
+            {
+                for (int i = 0; i < Convert.ToUInt16(txt_Numba_Amount.Text); i++)
+                {
+                    txt_output.Text += rnd.Next(/*Lowest Numba*/Convert.ToInt32(txt_Lowest_Numba.Text), /*Highest numba*/Convert.ToInt32(txt_Highest_Numba.Text)) + ", ";
+                }
+            }
+            catch
+            {
+                Console.WriteLine("euou. numba prolly incowekt");
+            }
+
+        }
     }
 }
